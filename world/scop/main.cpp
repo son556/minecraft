c29d2eaf1d6511c53969e8c71f7273bfb558b861
son556 <shhmun02@naver.com>
2024-08-13 20:48:28 +0900

    add delete block function and optimize chunk

--- world/scop/main.cpp
+++ world/scop/main.cpp
@@ -3,7 +3,6 @@
 #include "pch.h"
 #include "framework.h"
 #include "scop.h"
-#include "Game.h"
 
 
 // test
@@ -40,18 +39,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SCOP));
 
-    /*Game game;
-    game.Init(hWnd, 2);*/
-
-
-    //Test test(hWnd, 800, 650);
-    //test.setDrawBox();
-    //test.setDrawTexSkel();
-
+    // test code
     Terrain terrain(hWnd, 800, 650);
     Mat view = XMMatrixLookToLH(
-        vec3(0, 50, 50),
-        XMVector3Normalize(vec3(0, -1, -1.f)),
+        vec3(0, 50, -50),
+        XMVector3Normalize(vec3(0, -1, 1.f)),
         vec3(0, 1, 0)
     );
     Mat proj = XMMatrixPerspectiveFovLH(
@@ -70,11 +62,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     terrain.terrainsetVerticesAndIndices();
     finish = clock();
     cout << "time(ms) vertices and indices: " << static_cast<double>(finish - start) << endl;
-    start = clock();
-    terrain.readTerrainForTest();
-    finish = clock();
-    cout << "time(ms) read vertices and indices: " << static_cast<double>(finish - start) << endl;
+    terrain.updateTerrainForTest();
     terrain.setRender();
+    // test code
 
     MSG msg = {};
 
